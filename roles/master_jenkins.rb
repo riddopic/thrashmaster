c1acb5fad9c2ce9633f9a12b1999d01c99f39589
Stefano Harding <riddopic@gmail.com>
2015-03-03 12:33:13 -0800

    Master Jenkins is his own master, and Jenkins is his slave, the long pipeline to nowhere

---
+++ roles/master_jenkins.rb
@@ -0,0 +1,20 @@
+# encoding: UTF-8
+
+name        'jenkins_slave'
+description 'A Slave called Jenkins and his Pipeline'
+
+default_attributes(
+  java: {
+    install_flavor: 'oracle',
+    jdk_version: 7,
+    oracle: {
+      accept_oracle_download_terms: true
+    }
+  }
+)
+
+run_list %w[
+  recipe[java]
+  recipe[jenkins::slave]
+  recipe[git::default]
+]
