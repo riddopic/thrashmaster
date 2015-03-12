fcd96ae09aca2b2ef26048bceded62954027a2aa
Stefano Harding <riddopic@gmail.com>
2015-03-12 13:13:21 -0700

    have chef log from cron

--- roles/chef_client.rb
+++ roles/chef_client.rb
@@ -4,8 +4,13 @@ name        'chef_client'
 description 'Chef Client role'
 
 override_attributes(
-  chef_client: { init:     'none' },
-  cron:        { log_file: '/var/log/chef/client.log' }
+  chef_client: {
+    init: 'none',
+    cron: {
+      log_file:   '/var/log/chef/client.log',
+      append_log: true
+    }
+  }
 )
 
 run_list %w[
